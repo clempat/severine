275f1c9b4d5116fad5327de0cff26ffc2a654930
clempat <clement.patout@gmail.com>
2013-04-11 10:21:15 +0200

    create about

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -6,4 +6,32 @@
  * Time: 15:24
  * To change this template use File | Settings | File Templates.
  */
-echo 'about';
+?>
+<section id="page" class="container-fluid" style="margin-top: 20px;">
+    <div class="span10 offset1">
+        <h1>About</h1>
+        <div class="row">
+            <div class="span5">
+                <img src="<?php echo site_url('assets/img/about0.jpg')?>" width="100%" />
+                <img src="<?php echo site_url('assets/img/about2.jpg')?>" width="100%" />
+            </div>
+            <div class="span5">
+                <img src="<?php echo site_url('assets/img/about1.jpg')?>" width="100%" />
+                <p>
+                    Coming from France, I have worked as a journalist and filmmaker for different print media, televisions,
+                    institutions and NGOs from all over the world. I produce reports, short films, images films and
+                    documentary films in different countries (Europe, India, Nicaragua, Swaziland, Côte d’Ivoire, Ghana…).
+                </p>
+
+                <p>
+                    As a Video Journalist, I have substantial expertise in all aspects of Video Journalism including
+                    writing, interviewing, filming and editing. As a Filmmaker, I coordinate all the processes of making a
+                    film (from an initial story idea, through scriptwriting, shooting, editing and directing). As a Video
+                    trainer, I coach groups, help individuals in developing their media skills (how to use a camera, how to
+                    write a video story, how to edit a film…)
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
