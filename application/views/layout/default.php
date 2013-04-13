cf536155f32c12d7428ff08afc72a34e40534a7e
clempat <clement.patout@gmail.com>
2013-04-13 16:03:43 +0200

    update footer

--- application/views/layout/default.php
+++ application/views/layout/default.php
@@ -46,7 +46,7 @@
 <?php echo flash_message() ?>
 <?php echo $content_for_layout?>
 <footer class="footer">
-    © 2013. Séverine Lenglet, Journalist, Filmmaker, Video Trainer. | All Rights Reserved. | <a href="<?php echo site_url('mentions'); ?>">Impressum.</a>
+    © 2013. Séverine Lenglet, Video Journalism, Communication, Media Training. | All Rights Reserved. | <a href="<?php echo site_url('mentions'); ?>">Impressum.</a>
 
 </footer>
 </body>
