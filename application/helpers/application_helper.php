8bff299da038fb516ccb4619260dbd627f2342b7
clempat <clement.patout@gmail.com>
2013-04-14 02:09:35 +0200

    curl try1

--- application/helpers/application_helper.php
+++ application/helpers/application_helper.php
@@ -10,11 +10,13 @@
 function get_json($url) {
 
     $ch = curl_init();
+
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     $json = curl_exec($ch);
-
     $data = json_decode($json);
     return $data;
 
