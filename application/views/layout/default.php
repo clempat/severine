740ee87a278d1e66ecd905168979c167e47b6aea
clempat <clement.patout@gmail.com>
2013-06-21 08:32:48 +0200

    correct Google Analitycs

--- application/views/layout/default.php
+++ application/views/layout/default.php
@@ -39,21 +39,20 @@
     <link rel="shortcut icon" href="<?php echo site_url('assets/img/favicon.ico') ?>">
     <link rel="icon" type="image/gif" href="<?php echo site_url('assets/img/animated_favicon1.gif') ?>">
 
-    <script type="text/javascript">
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-41918631-1', 'severinelenglet.eu');
-        ga('send', 'pageview');
-
-    </script>
-
     <!-- JAVASCRIPT -->
     <?php echo $js_for_layout ?>
 </head>
 <body>
+<script type="text/javascript">
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-41918631-1', 'severinelenglet.eu');
+    ga('send', 'pageview');
+
+</script>
 <?php echo $menu_for_layout ?>
 
 <?php echo flash_message() ?>
