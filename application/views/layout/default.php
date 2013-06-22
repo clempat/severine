61eff8c6ca6f0115c520fe14f6c3b3edd8b657ca
clempat <clement.patout@gmail.com>
2013-06-22 12:49:47 +0200

    correct Google Analitycs

--- application/views/layout/default.php
+++ application/views/layout/default.php
@@ -42,16 +42,7 @@
     <!-- JAVASCRIPT -->
     <?php echo $js_for_layout ?>
 
-    <script type="text/javascript">
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-41918631-1', 'severinelenglet.eu');
-        ga('send', 'pageview');
-
-    </script>
 </head>
 <body>
 
