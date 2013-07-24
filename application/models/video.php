8e813292d206b4d80da0af2b7a7f23a8317aa47c
Clément Patout <clement.patout@gmail.com>
2013-07-24 19:37:29 +0200

    add image for btv

--- application/models/video.php
+++ application/models/video.php
@@ -230,7 +230,16 @@ class Video extends CI_Model {
 
                     break;
                 case 'www.tvbvideo.de':
-                    $thumbnail = "assets/img/nopic.jpg";
+                    $CI =& get_instance();
+                    $CI->load->library('simple_html_dom');
+                    $html = file_get_html($url_original);
+
+
+                    $r = html_entity_decode($html->find('#export_website_code', 0)->innertext());
+                    $r_html = str_get_html($r);
+                    preg_match('(http://api.kewego.com/video/getHTML5Thumbnail.+\"\))', $r_html, $thumbnail) ;
+                    $thumbnail = substr($thumbnail[0], 0, -2);
+                    $thumbnail = $this->get_final_url($thumbnail);
                     break;
                     default:
                         echo "Je ne connais pas ce site web... Veuillez vérifier le lien.";
@@ -281,5 +290,79 @@ class Video extends CI_Model {
         $result = $query->row();
 
         return $result->position;
+    }
+    /**
+     * get_redirect_url()
+     * Gets the address that the provided URL redirects to,
+     * or FALSE if there's no redirect.
+     *
+     * @param string $url
+     * @return string
+     */
+    function get_redirect_url($url){
+        $redirect_url = null;
+
+        $url_parts = @parse_url($url);
+        if (!$url_parts) return false;
+        if (!isset($url_parts['host'])) return false; //can't process relative URLs
+        if (!isset($url_parts['path'])) $url_parts['path'] = '/';
+
+        $sock = fsockopen($url_parts['host'], (isset($url_parts['port']) ? (int)$url_parts['port'] : 80), $errno, $errstr, 30);
+        if (!$sock) return false;
+
+        $request = "HEAD " . $url_parts['path'] . (isset($url_parts['query']) ? '?'.$url_parts['query'] : '') . " HTTP/1.1\r\n";
+        $request .= 'Host: ' . $url_parts['host'] . "\r\n";
+        $request .= "Connection: Close\r\n\r\n";
+        fwrite($sock, $request);
+        $response = '';
+        while(!feof($sock)) $response .= fread($sock, 8192);
+        fclose($sock);
+
+        if (preg_match('/^Location: (.+?)$/m', $response, $matches)){
+            if ( substr($matches[1], 0, 1) == "/" )
+                return $url_parts['scheme'] . "://" . $url_parts['host'] . trim($matches[1]);
+            else
+                return trim($matches[1]);
+
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * get_all_redirects()
+     * Follows and collects all redirects, in order, for the given URL.
+     *
+     * @param string $url
+     * @return array
+     */
+    function get_all_redirects($url){
+        $redirects = array();
+        while ($newurl = $this->get_redirect_url($url)){
+            if (in_array($newurl, $redirects)){
+                break;
+            }
+            $redirects[] = $newurl;
+            $url = $newurl;
+        }
+        return $redirects;
+    }
+
+    /**
+     * get_final_url()
+     * Gets the address that the URL ultimately leads to.
+     * Returns $url itself if it isn't a redirect.
+     *
+     * @param string $url
+     * @return string
+     */
+    function get_final_url($url){
+        $redirects = $this->get_all_redirects($url);
+        if (count($redirects)>0){
+            return array_pop($redirects);
+        } else {
+            return $url;
+        }
     }
 }
