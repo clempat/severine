fe16ccbeb3515bfc7eabaa4417582a0a9bffa9e5
clementpatout <clement@patataweb.com>
2013-09-27 21:04:08 +0200

    fix about

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -6,6 +6,9 @@
  * Time: 15:24
  * To change this template use File | Settings | File Templates.
  */
+?>
 
-echo $about->html;
+<section id="page" class="container" style="margin-top: 20px;">
+    <?php echo $about->html; ?>
+</section>
 
